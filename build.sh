0dfbb16a54f22177c85eaf9f90ec88a61ded6862
Long-belmont RW <dakogolgabriel@gmail.com>
2025-11-20 20:45:18 +0100

    more updates on build.sh

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@ pip install -r requirements.txt
 # 3. Collect Static Files (with our safety flags)
 python manage.py collectstatic --no-input --clear --no-post-process --ignore=cloudinary* --ignore=admin* --ignore=*.svg
 
-# 2. Install Node dependencies & Build Tailwind (Required for styles.css)
-python manage.py tailwind install
-python manage.py tailwind build
+# # 2. Install Node dependencies & Build Tailwind (Required for styles.css)
+# python manage.py tailwind install
+# python manage.py tailwind build
 
 # 4. Run Migrations
 python manage.py migrate
