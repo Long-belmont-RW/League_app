7b3826c66b61b62dfec7bf2ae501a80cce7921f7
Long-belmont RW <dakogolgabriel@gmail.com>
2025-11-20 11:55:54 +0100

    more patch fixes

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -o errexit
 pip install -r requirements.txt
 
 
-# Ignore BOTH cloudinary and admin files during collection/compression
-python manage.py collectstatic --no-input --clear --ignore=cloudinary* --ignore=admin*
+# We add --ignore=svg/* to skip compressing SVGs, which often cause this specific crash
+python manage.py collectstatic --no-input --clear --ignore=cloudinary* --ignore=admin* --ignore=*.svg
 python manage.py migrate
