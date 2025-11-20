a501dbbb0146eb19ba488ba1449607e377f4825f
Long-belmont RW <dakogolgabriel@gmail.com>
2025-11-20 22:00:11 +0100

    error fix

--- build.sh
+++ build.sh
@@ -2,18 +2,15 @@
 # exit on error
 set -o errexit
 
-# 1. Install Python dependencies
 pip install -r requirements.txt
 
+# 1. Manually delete the folder to force a fresh start
+rm -rf staticfiles
 
+# 2. Run collectstatic 
+# We remove --no-post-process because we switched to the safer storage backend
+# We keep --clear just in case
+# We ignore admin/cloudinary/svg to prevent crashes
+python manage.py collectstatic --no-input --clear --ignore=cloudinary* --ignore=admin* --ignore=*.svg
 
-
-# 3. Collect Static Files (with our safety flags)
-python manage.py collectstatic --no-input --clear --no-post-process --ignore=cloudinary* --ignore=admin* --ignore=*.svg
-
-# # 2. Install Node dependencies & Build Tailwind (Required for styles.css)
-# python manage.py tailwind install
-# python manage.py tailwind build
-
-# 4. Run Migrations
 python manage.py migrate
