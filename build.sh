1adcbd51dd2b4cff4abe8b46cca88366a1eb152b
Long-belmont RW <dakogolgabriel@gmail.com>
2025-11-20 22:31:00 +0100

    Add compiled CSS manually

--- build.sh
+++ build.sh
@@ -4,15 +4,12 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# 1. Build Tailwind (Crucial for styles.css)
-python manage.py tailwind install
-python manage.py tailwind build
+# --- TAILWIND DISABLED (Using the file we committed instead) ---
+# python manage.py tailwind install
+# python manage.py tailwind build
+# ---------------------------------------------------------------
 
-# 2. Clean up old files
-rm -rf staticfiles
-
-# 3. Collect Static Files
-# We keep --no-post-process to be safe against compression errors for now
+# Collect static files
 python manage.py collectstatic --no-input --clear --no-post-process --ignore=cloudinary* --ignore=admin* --ignore=*.svg
 
 python manage.py migrate
