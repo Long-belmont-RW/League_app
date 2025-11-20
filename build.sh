a5cb12860ce566ae49c83b0eb11b33ddbeb3f7d9
Long-belmont RW <dakogolgabriel@gmail.com>
2025-11-20 09:21:04 +0100

    Static debug

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input --ignore=cloudinary*
+# The --clear flag deletes the existing staticfiles folder before copying
+python manage.py collectstatic --no-input --clear --ignore=cloudinary*
 python manage.py migrate
