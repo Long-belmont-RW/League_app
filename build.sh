20dde6be83603d186bcd2bad8c0376f50ac08422
Long-belmont RW <dakogolgabriel@gmail.com>
2025-11-20 12:12:44 +0100

    working fix

--- build.sh
+++ build.sh
@@ -4,7 +4,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-
-# We add --ignore=svg/* to skip compressing SVGs, which often cause this specific crash
 python manage.py collectstatic --no-input --clear --ignore=cloudinary* --ignore=admin* --ignore=*.svg
 python manage.py migrate
